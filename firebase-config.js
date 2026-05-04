c0238468adb3d244c9aaaaa7f8a67fe940d5692c
antonin51400 <antonin51400@gmail.com>
2026-05-04 23:02:53 +0200

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'A_REMPLACER',
-  authDomain: 'A_REMPLACER.firebaseapp.com',
-  projectId: 'A_REMPLACER',
-  storageBucket: 'A_REMPLACER.appspot.com',
-  messagingSenderId: 'A_REMPLACER',
-  appId: 'A_REMPLACER'
+  apiKey: "AIzaSyCGgXgZ54gaMvXn53TA3cLFD2vDJLN1XVo",
+  authDomain: "horaire-bas-de-la-cense.firebaseapp.com",
+  projectId: "horaire-bas-de-la-cense",
+  storageBucket: "horaire-bas-de-la-cense.appspot.com", // corrigé
+  messagingSenderId: "175841833347",
+  appId: "1:175841833347:web:ab88daa78a66bc3ffec6b6"
 };
