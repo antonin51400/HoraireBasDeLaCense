40e454a8983e1c45b666a24d17bb9ffcec266259
antonin51400 <antonin51400@gmail.com>
2026-05-04 22:52:02 +0200

    Ajoute configuration Firebase

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'A_REMPLACER',
+  authDomain: 'A_REMPLACER.firebaseapp.com',
+  projectId: 'A_REMPLACER',
+  storageBucket: 'A_REMPLACER.appspot.com',
+  messagingSenderId: 'A_REMPLACER',
+  appId: 'A_REMPLACER'
+};
